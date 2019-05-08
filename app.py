f11ab6f95736284c26f2f0b72ba9bff5fce525b4
glyzinieh <50325323+glyzinieh@users.noreply.github.com>
2019-05-08 18:14:39 +0900

    add new file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-﻿import flask
+﻿import os
+
+import flask
 
 app = flask.Flask(__name__)
 
@@ -7,4 +9,5 @@ def index():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
